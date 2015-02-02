262596379b07418a541da6c986354674d631e2c0
Adam McArthur <adam@adammcarthur.net>
2015-02-03 02:12:00 +1100

    javascriptify sidebar + code cleanup

--- assets/javascripts/backend/feed.js
+++ assets/javascripts/backend/feed.js
@@ -1,16 +1,21 @@
 $(".timeAgo").timeago();
 
+// Active class first sidebar item on page load
+$(".answers li:first").addClass("active");
+
 // Loads a specific set of results (triggered from the sidebar)
 $(".answers li").on("click", function() {
+
   var query = $.ajax({
     url: "/feed/api?load=" + this.id,
     type: "GET"
   });
 
-  query.done(function(response) {
-    if ( response != undefined && response != "" && response != "Internal Server Error" ) {
-      //var submission = jQuery.parseJSON(response);
-      console.log(response);
+  query.done(function(submission) {
+    if ( submission != undefined && submission != "" ) {
+      console.log(submission);
+      $(".answers li").removeClass("active");
+      this.addClass("active");
     }
   });
 });
