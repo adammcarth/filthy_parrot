b80e9f61a75820f9d4e4a784b3d499d3e65c0d59
Adam McArthur <adam@adammcarthur.net>
2015-02-16 01:08:25 +1100

    Add production server stuff

---
+++ server/unicorn.rb
@@ -0,0 +1,19 @@
+# set path to app that will be used to configure unicorn,
+# note the trailing slash in this example
+@dir = "/var/www/"
+
+worker_processes 2
+working_directory @dir
+
+timeout 30
+
+# Specify path to socket unicorn listens to,
+# we will use this in our nginx.conf later
+listen "#{@dir}server/tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}server/tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}server/log/unicorn.stderr.log"
+stdout_path "#{@dir}server/log/unicorn.stdout.log"
